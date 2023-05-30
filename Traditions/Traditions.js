df7c9adebc3bb426b073edf64c3089084922addb
Mathias1212 <114069532+Mathias1212@users.noreply.github.com>
2023-05-30 10:34:45 +0200

    Create Traditions.js

---
+++ Traditions/Traditions.js
@@ -0,0 +1,8 @@
+
+  
+    function nextImage() {
+  images[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].classList.add('active');
+  setInterval(nextImage, 2000);
+}
